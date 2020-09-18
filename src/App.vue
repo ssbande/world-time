3919324b0da8e8852313910ca21b211f499c950a
ssbande <shreyasbande@shreyass-mbp.mynet>
2020-09-18 15:43:34 +0100

    task: initial commit :: create the app, importing assets and rendering background

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    Initial Commit.
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style lang="scss">
+@font-face {
+  font-family: "UnitedSans";
+  src: local("UnitedSans"),
+   url(./assets/fonts/UnitedSansCdBd.woff2) format("truetype");
+}
+
+@font-face {
+  font-family: "UnitedSansItalic";
+  src: local("UnitedSansItalic"),
+   url(./assets/fonts/UnitedItalicSmCdMd.woff2) format("truetype");
+}
+
+body {
+  background: #e60000;
+}
+#app {
+  font-family: UnitedSans, Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+}
+</style>
